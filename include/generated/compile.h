be75c49eba1d4d1b6c9e3d3362fbfc67ed29fbfe
Daniel Panzella <daniel.panzella@gmail.com>
2014-05-02 09:42:00 -0700

    initial commit with my config

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Tue Apr 29 19:45:58 PDT 2014"
+#define LINUX_COMPILE_BY "dpanzella"
+#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
